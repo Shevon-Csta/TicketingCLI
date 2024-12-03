3072c241037c43129ca6f9773aee970d48274ad6
Shevon-Csta <w2084398@westminster.ac.uk>
2024-12-04 01:07:39 +0530

    Began CLI Implementation

---
+++ src/com/start/ticketing/TicketingCLI.java
@@ -0,0 +1,8 @@
+package com.start.ticketing;
+import java.util.Scanner;
+public class TicketingCLI {
+    public static void main(String[] args) {
+        Scanner scanner= new Scanner(System.in);
+
+    }
+}
