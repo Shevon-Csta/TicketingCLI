ec196f31a4c5dc1ed93099bd33311ce4da8aaca0
Shevon-Csta <w2084398@westminster.ac.uk>
2024-12-04 01:08:05 +0530

    Defined Variables

--- src/com/start/ticketing/TicketingCLI.java
+++ src/com/start/ticketing/TicketingCLI.java
@@ -4,5 +4,11 @@ public class TicketingCLI {
     public static void main(String[] args) {
         Scanner scanner= new Scanner(System.in);
 
+        int totalTickets= 0;
+        int ticketReleaseRate=0;
+        int customerRetrievalRate=0;
+        int MaxTicketCapacity=0;
+
+        System.out.println("Welcome to Event Ticketing System!")
     }
 }
